4efcc3fcee7ac8f13617d92e44dd882a06a412ad
Nguyễn Hoàng Ân <an_nguyen@onskyinc.com>
2021-06-21 19:50:32 +0700

    Creating database for room database

---
+++ app/src/main/java/com/annguyenhoang/to_doapplicationlearning/data/ToDoDatabase.kt
@@ -0,0 +1,42 @@
+package com.annguyenhoang.to_doapplicationlearning.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ToDoData::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+        // Definition: Writes to this field are immediately
+        // made visible to other threads
+        @Volatile
+        // Nếu có nhiều Thread sử dụng cái instance
+        // này nó sẽ sử dụng version mới nhất
+        private var INSTANCE: ToDoDatabase? =null
+
+        fun getDatabase(context: Context): ToDoDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
