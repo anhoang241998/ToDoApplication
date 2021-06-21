0686c09625c32c0f37e6101d3dedb4729a814f4a
Nguyễn Hoàng Ân <an_nguyen@onskyinc.com>
2021-06-21 12:32:58 +0700

    Design add fragment UI

--- app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/list/ListFragment.kt
+++ app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/list/ListFragment.kt
@@ -1,8 +1,22 @@
 package com.annguyenhoang.to_doapplicationlearning.fragments.list
 
+import android.os.Bundle
+import android.view.View
+import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.annguyenhoang.to_doapplicationlearning.R
+import com.annguyenhoang.to_doapplicationlearning.databinding.FragmentListBinding
 
 class ListFragment : Fragment(R.layout.fragment_list) {
+
+    private val binding: FragmentListBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+    }
 
 }
