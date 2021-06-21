2e669498abcc65edee359938cd698d07e54aa2d3
Nguyễn Hoàng Ân <an_nguyen@onskyinc.com>
2021-06-21 12:59:36 +0700

    Design option menu for update fragment

--- app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/update/UpdateFragment.kt
@@ -1,8 +1,21 @@
 package com.annguyenhoang.to_doapplicationlearning.fragments.update
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.annguyenhoang.to_doapplicationlearning.R
 
 class UpdateFragment : Fragment(R.layout.fragment_update) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 
 }
