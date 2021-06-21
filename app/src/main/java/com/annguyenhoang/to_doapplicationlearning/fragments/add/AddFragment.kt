8c28296126f7e346fd65d9afb7cb114c0517c6ce
Nguyễn Hoàng Ân <an_nguyen@onskyinc.com>
2021-06-21 12:55:59 +0700

    Design option menu for add fragment

--- app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/add/AddFragment.kt
+++ app/src/main/java/com/annguyenhoang/to_doapplicationlearning/fragments/add/AddFragment.kt
@@ -1,8 +1,21 @@
 package com.annguyenhoang.to_doapplicationlearning.fragments.add
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.annguyenhoang.to_doapplicationlearning.R
 
 class AddFragment : Fragment(R.layout.fragment_add) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
 
 }
